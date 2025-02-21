d8fd5bda44fee07dbb0972920f89a6979de352a2
Maksim <max-goryachev@ya.ru>
2025-02-21 11:32:11 +0500

    qwe

--- pages/api/order.js
+++ pages/api/order.js
@@ -135,12 +135,15 @@ async function sendEmailNotification(data) {
     console.log("📩 Создание транспортера...");
 
     const transporter = nodemailer.createTransport({
-      service: "Gmail",
+      host: "smtp.mail.ru", // Используем SMTP Mail.ru
+      port: 465, // Порт для защищённого соединения
+      secure: true, // true для SSL
       auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
+        user: process.env.EMAIL_USER, // Твой email на Mail.ru
+        pass: process.env.EMAIL_PASS, // Сгенерированный пароль для SMTP
       },
     });
+    
 
     console.log("📩 Отправка email...");
 
