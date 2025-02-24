0895aa98309d4254eede2c2d57590f7c6d79fd9d
Maksim <max-goryachev@ya.ru>
2025-02-24 17:42:42 +0500

    метрика

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,12 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import { FaWhatsapp } from "react-icons/fa";
+import Script from "next/script"; // Импортируем next/script
 import "./globals.css";
 import { Manrope } from "next/font/google";
 
 const manrope = Manrope({
-  subsets: ["latin", "cyrillic"], // поддержка кириллицы
-  weight: ["300", "400", "500", "600", "700"], // нужные толщины
+  subsets: ["latin", "cyrillic"],
+  weight: ["300", "400", "500", "600", "700"],
   display: "swap",
 });
 
@@ -36,21 +37,51 @@ export default function RootLayout({ children }) {
     "openingHours": "Mo-Fr 10:00-19:00, Sa-Su 10:00-18:00",
     "url": "https://insurance-basova.vercel.app/"
   };
+
   return (
     <html lang="ru">
       <head>
-      <meta name="keywords" content="страхование Московская Славянка, ОСАГО Колпино, КАСКО Пушкин, страховой агент Санкт-Петербург, Ренессанс страхование, автостраховка онлайн, страховка оффлайн"/>
-      <meta name="description" content="Заполните форму, чтобы передать необходимые данные страхователю для оформления страхового полиса." />
-      <meta property="og:image" content="https://insurance-basova.vercel.app/favicon.ico"/>
-      <title>Страхование автомобилей в Московской Славянке</title>
-      <script
+        <meta name="keywords" content="страхование Московская Славянка, ОСАГО Колпино, КАСКО Пушкин, страховой агент Санкт-Петербург, Ренессанс страхование, автостраховка онлайн, страховка оффлайн"/>
+        <meta name="description" content="Заполните форму, чтобы передать необходимые данные страхователю для оформления страхового полиса." />
+        <meta property="og:image" content="https://insurance-basova.vercel.app/favicon.ico"/>
+        <title>Страхование автомобилей в Московской Славянке</title>
+
+        <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
         />
+
+        {/* Яндекс.Метрика */}
+        <Script
+          id="yandex-metrika"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+              (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+              ym(100031652, "init", {
+                  clickmap:true,
+                  trackLinks:true,
+                  accurateTrackBounce:true
+              });
+            `,
+          }}
+        />
       </head>
+      
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen`}
       >
+        {/* Носкрипт для Метрики */}
+        <noscript>
+          <div>
+            <img src="https://mc.yandex.ru/watch/100031652" style={{ position: "absolute", left: "-9999px" }} alt="" />
+          </div>
+        </noscript>
+
         <header className="w-full bg-[rgb(86,0,82)] border-b border-gray-300 py-4 px-6 justify-end items-center fixed hidden md:flex z-10">
           <div className="flex flex-col">
             <p className="text-white text-sm mb-1">
@@ -64,6 +95,7 @@ export default function RootLayout({ children }) {
             </div>
           </div>
         </header>
+
         <main className="flex-grow flex flex-col">{children}</main>
       </body>
     </html>
